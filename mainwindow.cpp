9d48c9a5c2883c8ee667b1ab935d0b27889af4ab
antonvw <a.m.vanwezenbeek@gmail.com>
2014-03-09 15:35:31 +0100

    removed incorrect slot

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -85,7 +85,6 @@ MainWindow::MainWindow(QWidget* parent, FractalWidget* fw)
   connect(colours_end, SIGNAL(triggered()), &m_fractalWidget->geometry(), SLOT(setColoursDialogEnd()));
   connect(images, SIGNAL(triggered()), &m_fractalWidget->geometry(), SLOT(setImages()));
   connect(copy, SIGNAL(triggered()), m_fractalWidget, SLOT(copy()));
-  connect(menu, SIGNAL(clicked()), this, SLOT(menu()));
   connect(newAction, SIGNAL(triggered()), this, SLOT(newFractalWidget()));
   connect(pause, SIGNAL(toggled(bool)), m_fractalWidget->renderer(), SLOT(pause(bool)));
   connect(qApp, SIGNAL(lastWindowClosed()), m_fractalWidget, SLOT(save()));
